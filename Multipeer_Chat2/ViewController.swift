9d839ebc601d3553962cbe28201f83332a56c954
yazuyazuya <kzyono-1999@docomo.ne.jp>
2019-02-09 14:32:50 +0900

    Initial Commit

---
+++ Multipeer_Chat2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multipeer_Chat2
+//
+//  Created by 大野和也 on 2019/02/09.
+//  Copyright © 2019 yazuyazuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
