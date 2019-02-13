5b29a08a2c23ab5953b038e77972cd4089b01fc9
yazuyazuya <kzyono-1999@docomo.ne.jp>
2019-02-13 12:01:10 +0900

    first commit

--- Multipeer_Chat2/Profile.swift
+++ Multipeer_Chat2/Profile.swift
@@ -32,6 +32,10 @@ class Profile: UIViewController, UIImagePickerControllerDelegate, UINavigationCo
             atomically: true,
             encoding: .utf8
         )
+        
+        // "Button"を押したらキーボード消える
+        enterName.endEditing(true)
+        
     }
     
     func imagePickerController(_ picker: UIImagePickerController,
@@ -58,6 +62,39 @@ class Profile: UIViewController, UIImagePickerControllerDelegate, UINavigationCo
     
     // キーボード関連 - ここから
     // Notification発行
+    func configureObserver() {
+        let notification = NotificationCenter.default
+        notification.addObserver(self, selector: #selector(keyboardWillShow(_:)),
+                                 name: UIResponder.keyboardWillShowNotification, object: nil)
+        notification.addObserver(self, selector: #selector(keyboardWillHide(_:)),
+                                 name: UIResponder.keyboardWillHideNotification, object: nil)
+        //print("Notificationを発行")
+    }
+    
+    // キーボードが表示時に画面をずらす。
+    @objc func keyboardWillShow(_ notification: Notification?) {
+        guard let rect = (notification?.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue,
+            let duration = notification?.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        UIView.animate(withDuration: duration) {
+            let transform = CGAffineTransform(translationX: 0, y: -(rect.size.height))
+            self.view.transform = transform
+        }
+        //print("keyboardWillShowを実行")
+    }
+    
+    /// キーボードが降りたら画面を戻す
+    @objc func keyboardWillHide(_ notification: Notification?) {
+        guard let duration = notification?.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? TimeInterval else { return }
+        UIView.animate(withDuration: duration) {
+            self.view.transform = CGAffineTransform.identity
+        }
+        //print("keyboardWillHideを実行")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     // ここまで
     
     
--- Multipeer_Chat2/ViewController.swift
+++ Multipeer_Chat2/ViewController.swift
@@ -121,6 +121,44 @@ class ViewController: UIViewController, MCNearbyServiceBrowserDelegate, MCSessio
         
     }
     
+    // キーボード関連 - ここから
+    // Notification発行
+    func configureObserver() {
+        let notification = NotificationCenter.default
+        notification.addObserver(self, selector: #selector(keyboardWillShow(_:)),
+                                 name: UIResponder.keyboardWillShowNotification, object: nil)
+        notification.addObserver(self, selector: #selector(keyboardWillHide(_:)),
+                                 name: UIResponder.keyboardWillHideNotification, object: nil)
+        //print("Notificationを発行")
+    }
+    
+    // キーボードが表示時に画面をずらす。
+    @objc func keyboardWillShow(_ notification: Notification?) {
+        guard let rect = (notification?.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue,
+            let duration = notification?.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        UIView.animate(withDuration: duration) {
+            let transform = CGAffineTransform(translationX: 0, y: -(rect.size.height))
+            self.view.transform = transform
+        }
+        //print("keyboardWillShowを実行")
+    }
+    
+    /// キーボードが降りたら画面を戻す
+    @objc func keyboardWillHide(_ notification: Notification?) {
+        guard let duration = notification?.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? TimeInterval else { return }
+        UIView.animate(withDuration: duration) {
+            self.view.transform = CGAffineTransform.identity
+        }
+        //print("keyboardWillHideを実行")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // ここまで
+    
+    
     @IBAction func sendChat(_ sender: Any) {
         // Bundle up the text in the message field, and send it off to all connected peers
         
@@ -142,6 +180,9 @@ class ViewController: UIViewController, MCNearbyServiceBrowserDelegate, MCSessio
         self.updateChat(jsonData: msg, fromPeer: self.peerID)
         
         self.messageField.text = ""
+        
+        // "Send"を押したらキーボード消える
+        messageField.endEditing(true)
         
     }
     
